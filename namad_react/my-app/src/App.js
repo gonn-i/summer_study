5abc2dce93da920542b26e7a7e1c7ef4579ac24e
김고은 <rlarhdms6828@naver.com>
2023-07-20 20:33:20 +0900

    노마드 react_ useEffect

--- namad_react/my-app/src/App.js
+++ namad_react/my-app/src/App.js
@@ -1,13 +1,45 @@
-import Button from "./button";
-import styles from "./App.module.css";
+import {useEffect, useState} from "react";
+
+function Hello () {
+  useEffect (()=> {
+    console.log("hi :)");
+    return ()=> {console.log("bye :(");}
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const[counter,setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
+
+  const onClick = () => setCounter((prev) => prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+  const onDiff = () => setShowing((prev) => !prev); 
+  console.log("i run all the time");
+
+  useEffect( () => {
+    console.log("CALL THE API ...")
+  }, []);
+
+  useEffect( () => {
+    console.log("I riun when 'keybord & counter' changes");
+  }, [keyword,counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Contunue"}/>
+      <input 
+        value={keyword} 
+        type="text" 
+        placeholder="Search here ..." 
+        onChange={onChange}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
+      <button onClick={onDiff}>{showing? "Hide" : "Show"}</button>
+      {showing? <Hello/>: null}
     </div>
-  );
-}
+  
+    );
+};
 
 export default App;
