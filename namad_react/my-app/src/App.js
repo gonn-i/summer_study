401647119a53564a43d397b2e103baaf17ebece9
김고은 <rlarhdms6828@naver.com>
2023-07-25 04:10:42 +0900

    노마드 React_To do list(2)

--- namad_react/my-app/src/App.js
+++ namad_react/my-app/src/App.js
@@ -1,8 +1,8 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 function App() {
  const [toDo, setToDo] = useState("");
- const [toDos, setToDos] = useState("");
+ const [toDos, setToDos] = useState([]);
  const onChange = (event) => {setToDo(event.target.value);}
 const onSubmited = (event) => {
      event.preventDefault()
@@ -18,6 +18,12 @@ const onSubmited = (event) => {
         <input value={toDo} onChange={onChange} type="text" placeholder="wrire your to do..."/>
         <button>Add To Do</button>
       </form>
+      <hr/>
+      <ul>
+        {toDos.map((item, index)=> (
+        <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   
     );
