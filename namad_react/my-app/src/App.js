97ac6baf772423b00c5496e6cb11ca489b11d3cf
김고은 <rlarhdms6828@naver.com>
2023-07-26 03:17:55 +0900

    노마드 7 예제 (대강 마무리)

--- namad_react/my-app/src/App.js
+++ namad_react/my-app/src/App.js
@@ -1,29 +1,23 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
- const [toDo, setToDo] = useState("");
- const [toDos, setToDos] = useState([]);
- const onChange = (event) => {setToDo(event.target.value);}
-const onSubmited = (event) => {
-     event.preventDefault()
-     if(toDo === "") return;
-     setToDo("");
-     setToDos((currentArray) => [toDo, ...currentArray])
-  }
+ const [loading, setLoading] = useState(true);
+ const [coins, setCoins] = useState([])
+ useEffect(()=> {
+  fetch("https://api.coinpaprika.com/v1/tickers")
+  .then((response)=> response.json())
+  .then((json)=>{ 
+    setCoins(json);
+    setLoading(false);
+  });
+ },[])
+ const [] = useState("");
+
  
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmited}>
-        <input value={toDo} onChange={onChange} type="text" placeholder="wrire your to do..."/>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((item, index)=> (
-        <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins!</h1>
+      {loading ? <strong>Loading...</strong>: null}
     </div>
   
     );
